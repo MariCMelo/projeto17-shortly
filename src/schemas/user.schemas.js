1db288b558d731e82837bda250a1885d426bb524
MariCMelo <marilia.correa.melo@gmail.com>
2023-08-06 21:41:16 -0300

    fixing validation

--- src/schemas/user.schemas.js
+++ src/schemas/user.schemas.js
@@ -3,7 +3,8 @@ import Joi from "joi"
 export const userSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
-    password: Joi.string().required().min(3)
+    password: Joi.string().min(3).required(),
+    confirmPassword: Joi.string().min(3).required().valid(Joi.ref("password")),
 });
 
 export const authSchema = Joi.object({
