2ea1ab9524418c01a5b3492385f9c7da7e07e431
MariCMelo <marilia.correa.melo@gmail.com>
2023-08-06 22:44:37 -0300

    fixing validation url

--- src/routes/shortUrl.routes.js
+++ src/routes/shortUrl.routes.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { urlSchema } from "../schemas/url.schemas.js";
 import { getUrlId, shortenUrl } from "../controllers/shortUrl.controller.js";
+import { validateAuth } from "../middlewares/validateAuth.js";
 
 const shortUrlRouter = Router();
 
-shortUrlRouter.post("/urls/shorten", validateSchema(urlSchema), shortenUrl)
+shortUrlRouter.post("/urls/shorten", validateSchema(urlSchema),validateAuth, shortenUrl)
 shortUrlRouter.get("/urls/:id", getUrlId) 
 
 export default shortUrlRouter
