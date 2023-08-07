77d7d3b618642ab2543b7eb73af250ace82075d0
MariCMelo <marilia.correa.melo@gmail.com>
2023-08-07 17:29:38 -0300

    refazendo dump

--- dump.sql
+++ dump.sql
@@ -2,10 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 15.3
--- Dumped by pg_dump version 15.3 (Ubuntu 15.3-1.pgdg20.04+1)
-
--- Started on 2023-08-06 21:23:52 -03
+-- Dumped from database version 12.14 (Ubuntu 12.14-0ubuntu0.20.04.1)
+-- Dumped by pg_dump version 12.14 (Ubuntu 12.14-0ubuntu0.20.04.1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -18,33 +16,23 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- TOC entry 5 (class 2615 OID 2200)
--- Name: public; Type: SCHEMA; Schema: -; Owner: -
---
-
--- *not* creating schema, since initdb creates it
-
-
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
 --
--- TOC entry 219 (class 1259 OID 16451)
 -- Name: sessions; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.sessions (
     id integer NOT NULL,
     token text NOT NULL,
-    "userId" integer,
-    "createdAt" timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+    "createdAt" timestamp without time zone DEFAULT now() NOT NULL,
+    "userId" integer
 );
 
 
 --
--- TOC entry 218 (class 1259 OID 16450)
 -- Name: sessions_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -58,8 +46,6 @@ CREATE SEQUENCE public.sessions_id_seq
 
 
 --
--- TOC entry 3166 (class 0 OID 0)
--- Dependencies: 218
 -- Name: sessions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -67,22 +53,20 @@ ALTER SEQUENCE public.sessions_id_seq OWNED BY public.sessions.id;
 
 
 --
--- TOC entry 217 (class 1259 OID 16425)
 -- Name: urls; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.urls (
     id integer NOT NULL,
-    "userId" integer,
     url text NOT NULL,
-    "shortUrl" text,
-    "createdAt" timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    "visitCount" integer
+    "shortUrl" text NOT NULL,
+    "visitCount" integer DEFAULT 0 NOT NULL,
+    "createdAt" timestamp without time zone DEFAULT now() NOT NULL,
+    "userId" integer
 );
 
 
 --
--- TOC entry 216 (class 1259 OID 16424)
 -- Name: urls_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -96,8 +80,6 @@ CREATE SEQUENCE public.urls_id_seq
 
 
 --
--- TOC entry 3167 (class 0 OID 0)
--- Dependencies: 216
 -- Name: urls_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -105,21 +87,19 @@ ALTER SEQUENCE public.urls_id_seq OWNED BY public.urls.id;
 
 
 --
--- TOC entry 215 (class 1259 OID 16414)
 -- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.users (
     id integer NOT NULL,
-    name character varying(256) NOT NULL,
-    email character varying(256) NOT NULL,
-    password character varying(256) NOT NULL,
-    createdat timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+    name text NOT NULL,
+    email text NOT NULL,
+    password text NOT NULL,
+    "createdAt" timestamp without time zone DEFAULT now() NOT NULL
 );
 
 
 --
--- TOC entry 214 (class 1259 OID 16413)
 -- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -133,8 +113,6 @@ CREATE SEQUENCE public.users_id_seq
 
 
 --
--- TOC entry 3168 (class 0 OID 0)
--- Dependencies: 214
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -142,7 +120,6 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
--- TOC entry 3006 (class 2604 OID 16454)
 -- Name: sessions id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -150,7 +127,6 @@ ALTER TABLE ONLY public.sessions ALTER COLUMN id SET DEFAULT nextval('public.ses
 
 
 --
--- TOC entry 3004 (class 2604 OID 16428)
 -- Name: urls id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -158,7 +134,6 @@ ALTER TABLE ONLY public.urls ALTER COLUMN id SET DEFAULT nextval('public.urls_id
 
 
 --
--- TOC entry 3002 (class 2604 OID 16417)
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -166,7 +141,53 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
--- TOC entry 3015 (class 2606 OID 16459)
+-- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.sessions VALUES (1, 'e51f7109-acc6-4fc0-84dc-06cfc989a4e2', '2023-05-24 19:48:01.21346', 2);
+INSERT INTO public.sessions VALUES (2, '2199c6c8-d3f5-4925-aac8-bdc474f9f66b', '2023-05-24 19:51:02.72433', 3);
+
+
+--
+-- Data for Name: urls; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.urls VALUES (1, 'https://www.google.com.br', '6e86659c', 3, '2023-05-24 20:10:09.669377', 3);
+INSERT INTO public.urls VALUES (4, 'https://www.driven.com.br', '18576fd8', 2, '2023-05-24 20:51:06.798758', 3);
+INSERT INTO public.urls VALUES (3, 'https://www.google.com.br', '546f914d', 0, '2023-05-24 20:13:50.520094', 1);
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.users VALUES (1, 'João', 'joao@driven.com.br', '$2b$10$VlHyUh/tA0ya8E7q7zWiSO1Fi0BiSDtcr7ciUurnF3kumt03z..Um', '2023-05-24 19:36:28.397363');
+INSERT INTO public.users VALUES (2, 'Maria', 'maria@driven.com.br', '$2b$10$tmPXIk7MlzVpG83vUz5hrOao9iDRUNRhio1KCil5tQqe6aevA96Vq', '2023-05-24 19:39:38.761389');
+INSERT INTO public.users VALUES (3, 'Letícia', 'let@driven.com.br', '$2b$10$d4423hX8DcpNClsdM6Q47urgodEpLjbB9lkTne2xC3qTmriYIB1rS', '2023-05-24 19:50:25.787345');
+
+
+--
+-- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.sessions_id_seq', 2, true);
+
+
+--
+-- Name: urls_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.urls_id_seq', 4, true);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 3, true);
+
+
+--
 -- Name: sessions sessions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -175,7 +196,6 @@ ALTER TABLE ONLY public.sessions
 
 
 --
--- TOC entry 3017 (class 2606 OID 16461)
 -- Name: sessions sessions_token_key; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -184,7 +204,6 @@ ALTER TABLE ONLY public.sessions
 
 
 --
--- TOC entry 3013 (class 2606 OID 16432)
 -- Name: urls urls_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -193,7 +212,14 @@ ALTER TABLE ONLY public.urls
 
 
 --
--- TOC entry 3009 (class 2606 OID 16423)
+-- Name: urls urls_shortUrl_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.urls
+    ADD CONSTRAINT "urls_shortUrl_key" UNIQUE ("shortUrl");
+
+
+--
 -- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -202,7 +228,6 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 3011 (class 2606 OID 16421)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -211,17 +236,21 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 3018 (class 2606 OID 16433)
--- Name: urls fk_users; Type: FK CONSTRAINT; Schema: public; Owner: -
+-- Name: sessions sessions_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id);
+
+
+--
+-- Name: urls urls_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.urls
-    ADD CONSTRAINT fk_users FOREIGN KEY ("userId") REFERENCES public.users(id);
+    ADD CONSTRAINT "urls_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id);
 
-
--- Completed on 2023-08-06 21:24:10 -03
 
 --
 -- PostgreSQL database dump complete
 --
-
--- src/controllers/shortUrl.controller.js
+++ src/controllers/shortUrl.controller.js
@@ -1,7 +1,7 @@
 import { db } from "../database/database.connection.js";
 import { customAlphabet } from "nanoid";
 
-const nanoid = customAlphabet(8);
+const nanoid = customAlphabet('1234567890abcdef', 8);
 
 export async function shortenUrl(req, res) {
   const { url } = req.body;
@@ -9,6 +9,7 @@ export async function shortenUrl(req, res) {
   const shortUrl = nanoid();
 
   try {
+   
     const {
       rows: [result],
     } = await db.query(
@@ -17,7 +18,7 @@ export async function shortenUrl(req, res) {
             RETURNING id, "shortUrl"`,
       [url, shortUrl, userId]
     );
-
+ console.log(result)
     res.status(201).send(result);
   } catch (err) {
     res.status(500).send(err.message);
